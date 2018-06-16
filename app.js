52ee55f8407068aaa8782e17896838048c9a0d29
Michael Hirschler <michael.vhirsch@gmail.com>
2018-06-16 18:11:37 +0200

    adds basic templates for webserver

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const port = process.env.NODE_PORT || 3000;
 
 // configure app
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // defines routes (should be outsourced if long)
 app.get('/', (req, res) => {
-  res.render('index', {name: 'World'});
+  res.render('pages/index', {name: 'World'});
+});
+
+app.get('/upload', (req, res) => {
+  res.render('pages/upload');
 });
 
 
