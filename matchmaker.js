ec09226f3b86ab6d561e2dc4fe39342dd1a5eec9
Stephan Kitzler-Walli <stephan@kitzler-walli.net>
2018-06-16 20:08:33 +0200

    Matchmaker limit 100 games in parallel

--- matchmaker.js
+++ matchmaker.js
@@ -3,67 +3,21 @@
 const game_1 = require("./game");
 const fs = require("fs");
 const Dockerode = require("dockerode");
-const mongodb_1 = require("mongodb");
+const mongodb = require("mongodb");
 const tarfs = require("tar-fs");
 const path = require("path");
 const WebRequest = require("web-request");
 const docker = new Dockerode({ socketPath: '/var/run/docker.sock' });
+const as = require("async");
 
-async function buildPlayerImages(rebuild = false, playerName = null) {
-	//iterate through all player folders
-	let players = fs.readdirSync(path.join(__dirname, "players"));
-	for (let i = 0; i < players.length; i++) {
-		let player = players[i].toLowerCase();
-		if(playerName != null){
-			playerName = playerName.toLowerCase();
-		}
-		
-		// skip the current folder if playerName does not match
-		if (playerName != null && player !== playerName) {
-			continue;
-		}
-		//check if image for player alread exists
-		let info = await docker.listImages({ filter: 'nodepirates/' + player });
-		if (info.length == 0) {
-			//image does not exist -> create image from player-folder
-			const pack = tarfs.pack(path.join(__dirname, 'players', player));
-			let stream = await docker.buildImage(pack, { t: 'nodepirates/' + player });
-			stream.pipe(process.stdout, {
-				end: true
-			});
-			stream.on('end', function () {
-				console.log("done");
-			});
-		}
-		else if (info.length > 1) {
-			//todo: delete all images and rebuild
-			console.log("Too many images");
-			for (let i = 0; i < info.length; i++) {
-				let image = docker.getImage(info[i].Id);
-				await image.remove();
-			}
-			await buildPlayerImages(false, player);
-		}
-		else {
-			//if rebuild is true -> delete -> rebuild
-			if (rebuild) {
-				let image = docker.getImage(info[0].Id);
-				await image.remove();
-				await buildPlayerImages(false, player);
-			}
-		}
-	}
-	console.log("done build");
-}
-
-async function Ready(player){
+async function Ready(port) {
 	let retryCount = 10;
-	while(retryCount > 0){
+	while (retryCount > 0) {
 		try {
-			let reset = await WebRequest.json('http://localhost:' + player.port + '/reset');
+			let reset = await WebRequest.json('http://localhost:' + port + '/reset');
 			return true;
 		}
-		catch(err){
+		catch (err) {
 			//console.log(err);
 			await sleep(1000);
 			retryCount--;
@@ -75,27 +29,32 @@ async function Ready(player){
 async function PlayGame(player1, player2) {
 	let container1, container2;
 	try {
-		container1 = await docker.createContainer({ Image: 'nodepirates/' + player1.name, ExposedPorts: { [player1.port + '/tcp']: {} }, HostConfig: { PortBindings: { [player1.port + '/tcp']: [{ 'HostPort': player1.port + '/tcp', 'HostIp': '127.0.0.1' }] } } });
-		container2 = await docker.createContainer({ Image: 'nodepirates/' + player2.name, ExposedPorts: { [player2.port + '/tcp']: {} }, HostConfig: { PortBindings: { [player2.port + '/tcp']: [{ 'HostPort': player2.port + '/tcp', 'HostIp': '127.0.0.1' }] } } });
+		container1 = await docker.createContainer({ Image: 'nodepirates/' + player1.name, HostConfig: { PortBindings: { [player1.port + '/tcp']: [{ 'HostIp': '127.0.0.1' }] } } });
+		container2 = await docker.createContainer({ Image: 'nodepirates/' + player2.name, HostConfig: { PortBindings: { [player2.port + '/tcp']: [{ 'HostIp': '127.0.0.1' }] } } });
 		await container1.start();
 		await container2.start();
+		const container1Data = await container1.inspect();
+		const container2Data = await container2.inspect();
+		const port1 = container1Data.NetworkSettings.Ports[Object.keys(container1Data.NetworkSettings.Ports)[0]][0].HostPort;
+		const port2 = container2Data.NetworkSettings.Ports[Object.keys(container2Data.NetworkSettings.Ports)[0]][0].HostPort;
+
 		// Check if the player is available by sending a reset call
-		if(! await Ready(player1)){
-			throw("Player1 did not answer in a timely fashion");
+		if (! await Ready(port1)) {
+			throw ("Player1 did not answer in a timely fashion");
 		}
-		if(! await Ready(player2)){
-			throw("Player2 did not answer in a timely fashion");
+		if (! await Ready(port2)) {
+			throw ("Player2 did not answer in a timely fashion");
 		}
 
 		try {
-			let game = new game_1.Game(player1.port, player2.port);
+			let game = new game_1.Game(port1, port2);
 			let result = await game.play();
 			console.log(result);
 		}
 		catch (gameErr) {
 			console.log(gameErr);
 		}
-		
+
 	}
 	catch (err) {
 		console.log(err);
@@ -110,43 +69,46 @@ async function PlayGame(player1, player2) {
 async function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function InsertPlayers() {
+
+async function StartWorldWar() {
+	//search for all events that have not been played yet
+	const start = process.hrtime();
 	try {
-		const client = await mongodb_1.MongoClient.connect("mongodb://127.0.0.1:27017/nodepirates");
+		const client = await mongodb.MongoClient.connect("mongodb://127.0.0.1:27017/nodepirates");
 		const db = await client.db("nodepirates");
-		const coll = await db.collection("players");
-		await coll.insertOne({
-			name: 'player1',
-			port: 8080
+		const eventsColl = await db.collection("events");
+		const playersColl = await db.collection("players");
+		const playersArr = await playersColl.find({}).toArray();
+		const players = playersArr.reduce(function (map, obj) {
+			map[obj.name] = obj;
+			return map;
+		}, {});
+		const events = await eventsColl.find({ played: false }).toArray();
+
+		const funcs = [];
+		for (let i = 0; i < events.length; i++) {
+			const event = events[i];
+			for (let j = 0; j < 100; j++) {
+				funcs.push(async () => {
+					console.log("playing game " + j + " betweeen " + event.player1 + ' and ' + event.player2);
+					const startGame = process.hrtime();
+					await PlayGame(players[event.player1], players[event.player2]);
+					const diffGame = process.hrtime(startGame);
+					console.log(diffGame);
+				});
+			}
+			//await eventsColl.updateOne({ '_id': event._id }, { $set: { played: true } });
+		}
+		as.parallelLimit(funcs,100,() =>{
+			client.close();
 		});
-		await coll.insertOne({
-			name: 'player2',
-			port: 8081
-		});
-		await coll.insertOne({
-			name: 'player-simple',
-			port: 8080
-		});
-		await coll.insertOne({
-			name: 'player-simple-core',
-			port: 5000
-		});
-		await client.close();
 	}
 	catch (err) {
 		console.log(err);
 	}
+	const diff = process.hrtime(start);
+	console.log(diff);
 }
-//buildPlayerImages(true);
 
-PlayGame(
-	{
-		name: 'player1',
-		environment: 'node:carbon-alpine',
-		port: 8080
-	}, {
-		name: 'player-simple-core',
-		environment: 'microsoft/dotnet:sdk',
-		port: 5000
-	}
-);
+
+StartWorldWar();
---
+++ player.js
@@ -0,0 +1,130 @@
+const mongodb = require("mongodb");
+const fs = require("fs");
+const Dockerode = require("dockerode");
+const mongodb_1 = require("mongodb");
+const tarfs = require("tar-fs");
+const path = require("path");
+const WebRequest = require("web-request");
+const docker = new Dockerode({ socketPath: '/var/run/docker.sock' });
+
+class Player {
+
+}
+Player.CreateNew = async function (zipFile, name, platform, port) {
+	try {
+		//todo: unzip player into folder
+		// create dockerfile for new player
+		// build docker image
+
+		const client = await mongodb.MongoClient.connect("mongodb://127.0.0.1:27017/nodepirates");
+		const db = await client.db("nodepirates");
+		const coll = await db.collection("players");
+
+		await coll.insertOne({
+			name: name,
+			port: port
+		});
+
+		const eventsColl = await db.collection("events");
+		const otherPlayers = await coll.find({name: {'$ne':name }}).toArray();
+		const events = [];
+
+		for(let i = 0;i< otherPlayers.length;i++){
+			events.push({ insertOne:{
+				player1: name,
+				player2: otherPlayers[i].name,
+				played: false
+			}});
+		}
+		
+		if(events.length){
+			await eventsColl.bulkWrite(events);
+		}
+		await client.close();
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+Player.InsertPlayers = async function() {
+	try {
+		await Player.CreateNew('', 'player1', 'node', 8080);
+		await Player.CreateNew('', 'player2', 'node', 8081);
+		//await Player.CreateNew('', 'player-simple', 'node', 8080);
+		await Player.CreateNew('', 'player-simple-core', 'dotnet', 5000);
+
+		// const client = await mongodb.MongoClient.connect("mongodb://127.0.0.1:27017/nodepirates");
+		// const db = await client.db("nodepirates");
+		// const coll = await db.collection("players");
+		// await coll.insertOne({
+		// 	name: 'player1',
+		// 	port: 8080
+		// });
+		// await coll.insertOne({
+		// 	name: 'player2',
+		// 	port: 8081
+		// });
+		// await coll.insertOne({
+		// 	name: 'player-simple',
+		// 	port: 8080
+		// });
+		// await coll.insertOne({
+		// 	name: 'player-simple-core',
+		// 	port: 5000
+		// });
+		// await client.close();
+	}
+	catch (err) {
+		console.log(err);
+	}
+}
+Player.buildPlayerImages = async function (rebuild = false, playerName = null) {
+	//iterate through all player folders
+	let players = fs.readdirSync(path.join(__dirname, "players"));
+	for (let i = 0; i < players.length; i++) {
+		let player = players[i].toLowerCase();
+		if (playerName != null) {
+			playerName = playerName.toLowerCase();
+		}
+
+		// skip the current folder if playerName does not match
+		if (playerName != null && player !== playerName) {
+			continue;
+		}
+		//check if image for player alread exists
+		let info = await docker.listImages({ filter: 'nodepirates/' + player });
+		if (info.length == 0) {
+			//image does not exist -> create image from player-folder
+			const pack = tarfs.pack(path.join(__dirname, 'players', player));
+			let stream = await docker.buildImage(pack, { t: 'nodepirates/' + player });
+			stream.pipe(process.stdout, {
+				end: true
+			});
+			stream.on('end', function () {
+				console.log("done");
+			});
+		}
+		else if (info.length > 1) {
+			//todo: delete all images and rebuild
+			console.log("Too many images");
+			for (let i = 0; i < info.length; i++) {
+				let image = docker.getImage(info[i].Id);
+				await image.remove();
+			}
+			await buildPlayerImages(false, player);
+		}
+		else {
+			//if rebuild is true -> delete -> rebuild
+			if (rebuild) {
+				let image = docker.getImage(info[0].Id);
+				await image.remove();
+				await buildPlayerImages(false, player);
+			}
+		}
+	}
+	console.log("done build");
+}
+
+exports.Player = Player;
+
+Player.InsertPlayers();
