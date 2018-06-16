414221ed5deb16b771e73ef5a3224e08f5433b21
Michael Hirschler <michael.vhirsch@gmail.com>
2018-06-16 19:31:53 +0200

    uploads form data to server

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const app = express();
 
 const port = process.env.NODE_PORT || 3000;
@@ -9,6 +11,8 @@ const port = process.env.NODE_PORT || 3000;
 // configure app
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }));
+app.use(fileUpload());
 
 // defines routes (should be outsourced if long)
 app.get('/', (req, res) => {
@@ -17,6 +21,17 @@ app.get('/', (req, res) => {
 
 app.get('/upload', (req, res) => {
   res.render('pages/upload');
+});
+
+app.post('/upload', (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded');
+  }
+
+  res.send('ok');
 });
 
 
