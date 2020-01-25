b270eb53a5e579ab651ca8eaf7b2349fc2bd6b6a
Matthias Ableidinger <ableimat@gmx.at>
2020-01-25 23:41:00 +0100

    don't duplicate events in the events collection, reset the "done" flag

--- player.js
+++ player.js
@@ -61,21 +61,29 @@ class PlayerFacade {
 	 */
 	async enqueue(name) {
 		//TODO lock
+		let eventSelector = {'$or': [{player1: name}, {player2: name}]};
 		const otherPlayers = await this.players.find({name: {'$ne': name}}).toArray();
-		const events = [];
+		await this.events.updateMany(eventSelector, {$set: {played: false}});
+		const existingEvents = await this.events.find(eventSelector).toArray();
+		const existingEventPlayers = existingEvents.map(event => {
+			return event.player1 === name ? event.player2 : event.player1;
+		});
 
+		const newEvents = [];
 		for (let i = 0; i < otherPlayers.length; i++) {
-			events.push({
-				insertOne: {
-					player1: name,
-					player2: otherPlayers[i].name,
-					played: false
-				}
-			});
+			if (!existingEventPlayers.includes(otherPlayers[i].name)) {
+				newEvents.push({
+					insertOne: {
+						player1: name,
+						player2: otherPlayers[i].name,
+						played: false
+					}
+				});
+			}
 		}
 
-		if (events.length) {
-			await this.events.bulkWrite(events);
+		if (newEvents.length) {
+			await this.events.bulkWrite(newEvents);
 		}
 	}
 
