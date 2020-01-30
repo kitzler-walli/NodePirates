658ce2563e15cd61efe9c9ace72f4392fbe91c48
Matthias Ableidinger <Matthias.Ableidinger@catalysts.cc>
2020-01-30 19:41:31 +0100

    fix insert-players script

--- bin/insert-players.js
+++ bin/insert-players.js
@@ -6,16 +6,17 @@ const Player = require('../player');
 const fs = require('fs');
 const path = require('path');
 
-function buildPremadePlayerImages(player) {
+async function buildPremadePlayerImages(player) {
 	//iterate through all player folders
 	let playerNames = fs.readdirSync(path.join(__dirname, "../players"));
 	for (let i = 0; i < playerNames.length; i++) {
 		let playerName = playerNames[i].toLowerCase();
-		player.rebuildPlayerImage(path.join(__dirname, '../players', playerName), playerName); //no await
+		await player.rebuildPlayerImage(path.join(__dirname, '../players', playerName), playerName); //no await
 	}
 }
 
 (async () => {
+	console.log("starting...");
 	const client = await mongo.connect();
 	const player = await Player.initFacade(client.db('nodepirates'));
 
@@ -28,6 +29,8 @@ function buildPremadePlayerImages(player) {
 	await player.players.insertOne({name: 'player-simple-core'});
 	await player.enqueue('player-simple-core');
 
+	console.log("building images...");
 	await buildPremadePlayerImages(player);
 	await client.close();
+	console.log("done!");
 })();
